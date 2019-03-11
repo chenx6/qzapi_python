a041a48d561db8d4e1ce39a66ed43c970538d522
chenx6 <abc82766@gmail.com>
2019-03-11 17:04:09 +0800

    change example.py to a interactive program

--- example.py
+++ example.py
@@ -1,26 +1,42 @@
-import qzAPI
+import qzapi
+from getpass import getpass
 
-idNumber = input('输入账号')
-password = input('输入密码')
+def menu(qz):
+    help = '1.查看个人信息\n2.查询本周课表\n3.获取空自习室\n'
+    help += '4.查询成绩\n'
+    choice = input(help)
+    while choice != 'quit':
+        if choice is '1':
+            info = qz.get_user_info()
+            formatInfo = f'{info["name"]}, {info["sex"]}, {info["grade"]}, '
+            formatInfo += f'{info["college"]}, {info["major"]}, {info["class"]}'
+            print(formatInfo)
+        elif choice is '2':
+            sch = qz.get_schedule()
+            for i in sch:
+                formatSch = f'{i["weekday"]}, {i["daySequence"]}, {i["subjectName"]}, '
+                formatSch += f'{i["roomName"]}, {i["teacherName"]}, {i["teachWeek"]}'
+                print(formatSch)
+        elif choice is '3':
+            period = input('输入时间段("am","pm","night","allday"):')
+            room = qz.get_empty_classroom('am')
+            for i in room:
+                print(f'{i["floorName"]}:')
+                for j in i['roomList']:
+                    print(f'{j["roomName"]}, {j["roomSize"]}, {j["floorName"]}')
+        elif choice is '4':
+            semester = input('输入学期(例:2018-2019-1):')
+            score = qz.get_exam_score(semester)
+            for i in score:
+                formatScore = f'{i["subjectProp"]}, {i["subjectCategory"]}, '
+                formatScore += f'{i["subjectEngName"]}, {i["subjectName"]}, '
+                formatScore += f'{i["score"]}, {i["credit"]}'
+                print(formatScore)
+        choice = input(help)
 
-qz = qzAPI.qzapi(idNumber, password)
+if __name__ == '__main__':
+    idNumber = input('输入账号:')
+    password = getpass('输入密码:')
 
-sch = qz.get_schedule()
-for i in sch:
-    print('%s %s %s %s %s %s' % (i['weekday'], i['daySequence'], i['subjectName'], i['roomName'],
-                                 i['teacherName'],  i['teachWeek']))
-
-info = qz.get_user_info()
-print('%s %s %s %s %s %s' %
-      (info['name'], info['sex'], info['grade'], info['college'], info['major'], info['class']))
-
-score = qz.get_exam_score('2018-2019-1')
-for i in score:
-    print('%s %s %s %s %s %s' % (
-        i['subjectProp'], i['subjectCategory'], i['subjectEngName'], i['subjectName'], i['score'], i['credit']))
-
-room = qz.get_empty_classroom('am')
-for i in room:
-      print('%s:'%i['floorName'])
-      for j in i['roomList']:
-            print('%s %s %s'%(j['roomName'], j['roomSize'], j['floorName']))
+    qz = qzapi.qzapi(idNumber, password)
+    menu(qz)
